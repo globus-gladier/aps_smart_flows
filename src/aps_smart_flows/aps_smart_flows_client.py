c5e5a64f7254994ddb83ba1c376f863987bb40e1
Rafael Vescovi <ravescovi@gmail.com>
2024-07-09 21:48:53 -0500

    working version

--- src/aps_smart_flows/aps_smart_flows_client.py
+++ src/aps_smart_flows/aps_smart_flows_client.py
@@ -5,19 +5,22 @@
 import uuid
 from pprint import pprint
 
+from diaspora_event_sdk import Client as DiasporaGlobusClient
 from gladier import GladierBaseClient, generate_flow_definition
 from tools.consume_event import Diaspora_Consume_Event
 from tools.produce_event import Diaspora_Produce_Event
 from tools.sleep_tool import SleepTool
 from tools.sys_info_tool import SysInfoTool
 
+TOPIC_NAME = "aps_smart_flow"
+GLOBUS_GROUP = "0bbe98ef-de8f-11eb-9e93-3db9c47b68ba"
 
-@generate_flow_definition(
-    # output of  sysinfo >> input of diaspora
-)
+
+@generate_flow_definition()
 class ProduceClient(GladierBaseClient):
     """"""
 
+    globus_group = [GLOBUS_GROUP]
     gladier_tools = [SysInfoTool, Diaspora_Produce_Event]
 
 
@@ -25,6 +28,7 @@ class ProduceClient(GladierBaseClient):
 class ConsumeClient(GladierBaseClient):
     """"""
 
+    globus_group = [GLOBUS_GROUP]
     gladier_tools = [SleepTool, Diaspora_Consume_Event]
 
 
@@ -33,20 +37,14 @@ def generate_unique_key():
     return str(uuid.uuid4())
 
 
-def create_new_topic():
-    """"""
-    pass
-
-
-def create_flow_control_topic():
-    """"""
-    pass
-
-
 def run_flow(**kwargs):
     """"""
     ClientFlow1 = ProduceClient()
     ClientFlowConsumer = ConsumeClient()
+    ClientDiaspora = DiasporaGlobusClient()
+    print(ClientDiaspora.register_topic(TOPIC_NAME))
+    print(ClientDiaspora.list_topics())
+    print("Topic to produce/consume:", TOPIC_NAME)
 
     new_key = generate_unique_key()
     flow_input = {
@@ -54,8 +52,8 @@ def run_flow(**kwargs):
             ##compute bits
             "compute_endpoint": "36d0b3c2-47a8-4465-8742-8296dc266b0b",
             "wait_time": 15,
-            "topic": "topic7b385f033313",
-            "msgs": [{}],
+            "topic": TOPIC_NAME,
+            "msgs": [{"smartkey": new_key}],
         }
     }
 
@@ -73,9 +71,7 @@ def run_flow(**kwargs):
     trigger_id = flow_run["action_id"]
     flow_input["input"]["filters"] = [
         {"Pattern": {"value": {"flow_run_id": [{"prefix": str(trigger_id[0:5])}]}}},
-    ]  ##bug here but it works
-    ##
-    print(trigger_id)
+    ]
 
     ##This flow will wait for all the flows on trigger flows
     flow_run = ClientFlowConsumer.run_flow(
